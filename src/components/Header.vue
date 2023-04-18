aa843d7eea880fc352272dbb89f58ec2635ce506
mirzi <mirzi@gmail.com>
2023-04-18 19:50:32 +0500

    asd

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+        <div class="container">
+            <div class="header__wrap">
+                <a href="#" class="logo">PROWEB</a>
+                <ul class="menu">
+                    <li><a href="#" class="menu__link">Обо мне</a></li>
+                    <li><a href="#" class="menu__link">Мои работы</a></li>
+                    <li><a href="#" class="menu__link">Контакты</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <section class="banner">
+        <div class="banner__wrap">
+            <h1 class="banner__h">Добро пожаловать на мой сайт</h1>
+            <button class="banner__btn">Позвонить</button>
+        </div>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
